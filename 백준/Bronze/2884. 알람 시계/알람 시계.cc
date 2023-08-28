eeed40f54bf06dc22cec75a725c73af3e1ac19b9
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-08-28 16:15:15 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	b = b - 45;
+	if (b<0)
+	{
+		b = b + 60;
+		a = a - 1;
+	}
+	if (a<0)
+	{
+		a = a + 24;
+	}
+	printf("%d %d",a,b);
+	return 0;
+}
