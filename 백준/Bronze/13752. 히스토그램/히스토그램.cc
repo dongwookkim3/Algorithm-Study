c32bcec9f4f72deb9ecd380fef796a2f148f4620
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-08-28 16:50:58 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	for (;a>0;a--)
+	{
+		scanf("%d",&b);
+		for (;b>0;b--)
+		{
+			printf("=");
+		}
+		printf("\n");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
