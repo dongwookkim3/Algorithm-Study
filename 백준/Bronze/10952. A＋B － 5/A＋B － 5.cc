e4a154861dee5922cfdc5f2407e6e4e0a55fea72
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-08-28 16:39:13 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    while(scanf("%d %d", &A, &B) != EOF)
+    {
+    	if (A == 0 && B == 0)
+		{
+			break;
+		}
+        printf("%d\n", A+B);
+        
+    }
+    return 0;
+}
