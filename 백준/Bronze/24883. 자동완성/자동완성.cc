0ad8b82b26143043fa2476318c6296e8f772d88c
김동욱 <128461452+dongwookkim3@users.noreply.github.com>
2023-09-01 11:21:40 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	char a;
+	scanf("%c",&a);
+	if (a=='N') printf("Naver D2");
+	else if (a=='n') printf("Naver D2");
+	else printf("Naver Whale");
+	return 0;
+}
