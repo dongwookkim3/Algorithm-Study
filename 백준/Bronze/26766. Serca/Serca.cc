91018b7b0b7a2504b1287f97fb1df14d23ac042b
김동욱 <128461452+dongwookkim3@users.noreply.github.com>
2023-09-01 10:42:58 +0900

    [Bronze V] Title: Serca, Time: 132 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	for (;a>0;a--)
+	{
+		printf(" @@@   @@@ \n");
+		printf("@   @ @   @\n");
+		printf("@    @    @\n");
+		printf("@         @\n");
+		printf(" @       @ \n");
+		printf("  @     @  \n");
+		printf("   @   @   \n");
+		printf("    @ @    \n");
+		printf("     @     \n");
+	}
+	
+	return 0;
+}
