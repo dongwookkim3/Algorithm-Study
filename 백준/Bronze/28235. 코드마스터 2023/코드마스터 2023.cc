7c5fe975a0c2eae6830dfbbf415edaa7971ced0e
김동욱 <128461452+dongwookkim3@users.noreply.github.com>
2023-09-01 10:38:32 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	char A;
+	scanf("%c",&A);
+	if (A=='S') printf("HIGHSCHOOL");
+	else if (A=='C') printf("MASTER");
+	else if (A=='2') printf("0611");
+	else if (A=='A') printf("CONTEST");
+	return 0;
+}
