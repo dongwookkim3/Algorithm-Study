84e5e6f6de5737872fdafffb925a5d0bd8f13599
김동욱 <128461452+dongwookkim3@users.noreply.github.com>
2023-09-01 10:39:19 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char a;
+	scanf("%c",&a);
+	if (a=='N') printf("North London Collegiate School");
+	if (a=='B') printf("Branksome Hall Asia");
+	if (a=='K') printf("Korea International School");
+	if (a=='S') printf("St. Johnsbury Academy");
+	return 0;
+}
