195b86fc5c2a2558266d17030f54bd6786a52915
김동욱 <128461452+dongwookkim3@users.noreply.github.com>
2023-09-01 11:15:03 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,T;
+	scanf("%d\n%d\n%d",&a,&b,&c);
+	T=a*1+b*2+c*3;
+	if (T>=10) printf("happy");
+	else printf("sad");
+	
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}
