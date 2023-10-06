049d078338ed8e6a7bb2abb81835d21a54a83505
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-10-06 21:36:58 +0900

    [Bronze IV] Title: Simple Sum, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	char A,B;
+	cin >> a;
+	cin >> A;
+	cin >> b;
+	cin >> B;
+	cin >> c;
+	if (a+b==c) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
