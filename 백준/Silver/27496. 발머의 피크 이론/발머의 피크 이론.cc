a2a0d3624045ed508d0152b5ec27c6db2d3a22f8
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-10-21 15:08:34 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 76 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
@@ -10,9 +10,6 @@ int main(){
 	for (int i=1;i<=n;i++){
 		cin >> A[i];
 		A[i]+=A[i-1];
-	}
-	for (int i=1;i<=n;i++){
-		//cout << A[i]-A[i-k] << "\n";
 		if (A[i]-A[max(0,i-k)]>=129 && A[i]-A[max(0,i-k)]<=138){
 			m++;
 		}
