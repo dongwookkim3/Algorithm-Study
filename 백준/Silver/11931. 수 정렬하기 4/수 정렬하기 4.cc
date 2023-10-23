942b16a325ed30ad47e68528756946ff619987da
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-10-23 17:10:53 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 672 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	int n,A[1000001]={};
+	cin >> n;
+	for (int i=0;i<n;i++){
+		cin >> A[i];
+	}
+	sort(A,A+n);
+	for (int i=n-1;i>=0;i--){
+		cout << A[i] << "\n";
+	}
+	return 0;
+}
