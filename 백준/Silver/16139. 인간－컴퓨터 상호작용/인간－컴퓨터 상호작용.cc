bf38caf7fb37c639d9106fdb2b9874b688f13b18
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-10-28 23:36:06 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 2116 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+	/*
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	*/
+	int A[27][2001]={};
+	string s;
+	getline(cin,s);
+	for (int i=0;i<s.length();i++){
+		A[s[i]-96][i]++;
+		for (int j=1;j<=26;j++){
+			A[j][i]+=A[j][i-1];
+		}
+	}
+	/*
+	for (int i=0;i<s.length();i++){
+		for (int j=1;j<=26;j++){
+			cout << A[j][i] << " ";
+		}
+		cout << "\n";
+	}
+	*/
+	int n;
+	cin >> n;
+	while(n--){
+		char w;
+		int a,b;
+		cin >> w >> a >> b;
+		cout << A[w-96][b]-A[w-96][--a] << "\n";
+	}
+	return 0;
+}
