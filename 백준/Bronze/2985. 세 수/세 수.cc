ff2355cc895f7a6f2db1ae3c74c40d1b988d8a8f
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-11-16 19:26:58 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int A,B,C;
+	cin >> A >> B >> C;
+	if (A+B==C) cout << A << '+' << B << '=' << C;
+	else if (A==B+C) cout << A << '=' << B << '+' << C;
+	else if (A-B==C) cout << A << '-' << B << '=' << C;
+	else if (A==B-C) cout << A << '=' << B << '-' << C;
+	else if (A*B==C) cout << A << '*' << B << '=' << C;
+	else if (A==B*C) cout << A << '=' << B << '*' << C;
+	else if (A/B==C) cout << A << '/' << B << '=' << C;
+	else if (A==B/C) cout << A << '=' << B << '/' << C;
+	return 0;
+}
