d5f4a1e2aa9a1c0ad570cf1752bec75adeeada4b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-11-28 16:05:20 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>> n;
+	for (int i=1;i<=n;i++){
+		for (int j=1;j<=n;j++){
+			cout << "@@@@@";
+		}
+		cout << "\n";
+	}
+	for (int i=1;i<=n;i++){
+		for (int j=1;j<=n;j++){
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i=1;i<=n;i++){
+		for (int j=1;j<=n;j++){
+			cout << "@@@@@";
+		}
+		cout << "\n";
+	}
+	for (int i=1;i<=n*2;i++){
+		for (int j=1;j<=n;j++){
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
