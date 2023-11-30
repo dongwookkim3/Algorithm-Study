696a12716d603dae20dbf3af808f77dfa720e69c
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-01 00:18:59 +0900

    [Bronze III] Title: Dyslexia, Time: 8 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Bronze/8371. Dyslexia/Dyslexia.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,m=0;
+	string A,B;
+	cin >> n;
+	getchar();
+	getline(cin,A);
+	getline(cin,B);
+	for (int i=0;i<n;i++){
+		if (A[i]!=B[i]) m++;
+	}
+	cout << m;
+	return 0;
+}
