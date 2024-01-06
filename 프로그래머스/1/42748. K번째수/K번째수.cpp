3ee099b803e8ac6b84869f2795369c2467e44d31
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 20:49:36 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.08 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands){
+    vector<int> answer;
+    for (int i=0;i<commands.size();i++){
+        vector<int> A;
+        for (int j=commands[i][0]-1;j<commands[i][1];j++){
+            A.push_back(array[j]);
+        }
+        sort(A.begin(),A.end());
+        answer.push_back(A[commands[i][2]-1]);
+    }
+    return answer;
+}
