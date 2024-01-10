fb80475b6a39ec2ecc0af69ed2e28cd64ba0e14b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-10 21:11:56 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+	queue<int> A;
+	int n;
+	cin >> n;
+	for (int i=1;i<=n;i++){
+		A.push(i);
+	}
+	for (int i=2;A.size()!=1;i++){
+		if (i%2!=0) A.push(A.front());
+		else cout << A.front() << ' ';
+		A.pop();
+	}
+	cout << A.front();
+	return 0;
+}
