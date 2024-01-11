c613a35e3110ecd52963277f516d29d5e9833652
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-12 01:06:11 +0900

    [level Lv.0] Title: 문자열 돌리기, Time: 2.87 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void){
+    string A;
+    cin >> A;
+    for (int i=0;i<A.length();i++){
+        cout << A[i] << "\n";
+    }
+    return 0;
+}
