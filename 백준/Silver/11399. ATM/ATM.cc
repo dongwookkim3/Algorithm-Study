3b3f59552f8ac77262a895bd3bd621f6d048e2e2
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-12 22:10:48 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000
+using namespace std;
+int main(){
+	int n,m=0;
+	int A[MAX+1]={};
+	cin >> n;
+	for (int i=1;i<=n;i++){
+		cin >> A[i];
+	}
+	sort(A+1,A+n+1,greater<int>());
+	for (int i=1;i<=n;i++){
+		m+=A[i]*i;
+	}
+	cout << m;
+	return 0;
+}
