0791c8a349e6253b0e803c3923edad8d68f3461d
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-12 23:01:53 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 212 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -2,6 +2,9 @@
 #include <map>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     map<string,string> A;
     int n,m;
     cin >> n >> m;
