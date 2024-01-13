0ea7d98e2ee73a4952a31867212f0b67e72a7afb
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 15:57:30 +0900

    [level Lv.0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+int solution(string ineq,string eq,int n,int m){
+    if (ineq==">"){
+        if (eq=="="){
+            return (n>=m);
+        }
+        else {
+            return (n>m);
+        }
+    }
+    else {
+        if (eq=="="){
+            return (n<=m);
+        }
+        else {
+            return (n<m);
+        }
+    }
+}
