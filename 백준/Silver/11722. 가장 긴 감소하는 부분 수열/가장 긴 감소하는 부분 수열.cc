a7ad6c088747cb37322521cba550bfb7a91a5eed
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-14 14:33:02 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#define MAX 1000
+using namespace std;
+int main(){
+    int n,m=0;
+    int A[MAX+1]={};
+    int B[MAX+1]={};
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cin >> A[i];
+        for (int j=1;j<i;j++){
+            if (B[j]>=B[i] && A[i]<A[j]){
+                B[i]=B[j]+1;
+            }
+        }
+        if (m<B[i]){
+            m=B[i];
+        }
+    }
+    cout << ++m;
+    return 0;
+}
