e2c89f8f11e0ba21a4cf60307824029960674974
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-17 00:54:28 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    priority_queue<long long> pq;
+    int n;
+    cin >> n;
+    while(n--){
+        long long a;
+        cin >> a;
+        if (a==0){
+            if (pq.size()){
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+            else {
+                cout << "0\n";
+            }
+        }
+        else {
+            pq.push(a);
+        }
+    }
+    return 0;
+}
