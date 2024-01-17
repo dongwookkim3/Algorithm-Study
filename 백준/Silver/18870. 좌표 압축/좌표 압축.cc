5ab43d7d1fa06940a0201ad1ec322a438f53e430
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-17 22:57:42 +0900

    [Silver II] Title: 좌표 압축, Time: 484 ms, Memory: 10152 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -18,6 +18,9 @@ int search(int t){
     return l;
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n;
     cin >> n;
     for (int i=0;i<n;i++){
