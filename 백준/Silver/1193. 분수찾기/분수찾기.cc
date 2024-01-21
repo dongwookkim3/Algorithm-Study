a3073cf0f2914e24381e303f7d0feb0422fb0c2d
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-21 16:48:27 +0900

    [Silver V] Title: 분수찾기, Time: 152 ms, Memory: 80056 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -1,23 +1,26 @@
 #include <iostream>
+
 #define MAX 4472
 #define XMAX 10000000
 using namespace std;
+
 int main(void) {
-    int x=1,y=1,d=0;
-    int dx[2]={-1,1};
-    int dy[2]={1,-1};
-    int A[MAX+1][MAX+1]={};
-    for (int i=1;i<=XMAX;i++){
-        A[x][y]=i;
-        if (x+dx[d%2]!=0 && y+dy[d%2]!=0){
-            x+=dx[d%2];
-            y+=dy[d%2];
-        }
-        else {
-            if (d%2==0){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int x = 1, y = 1, d = 0;
+    int dx[2] = {-1, 1};
+    int dy[2] = {1, -1};
+    int A[MAX + 1][MAX + 1] = {};
+    for (int i = 1; i <= XMAX; i++) {
+        A[x][y] = i;
+        if (x + dx[d % 2] != 0 && y + dy[d % 2] != 0) {
+            x += dx[d % 2];
+            y += dy[d % 2];
+        } else {
+            if (d % 2 == 0) {
                 y++;
-            }
-            else {
+            } else {
                 x++;
             }
             d++;
@@ -25,13 +28,12 @@ int main(void) {
     }
     int n;
     cin >> n;
-    for (int i=1;i<=MAX;i++){
-        for (int j=1;j<=MAX;j++){
-            if (A[i][j]==n){
-                cout << i<< '/' << j;
+    for (int i = 1; i <= MAX; i++) {
+        for (int j = 1; j <= MAX; j++) {
+            if (A[i][j] == n) {
+                cout << i << '/' << j;
                 return 0;
-            }
-            else if (A[i][j]>n){
+            } else if (A[i][j] > n) {
                 break;
             }
         }
