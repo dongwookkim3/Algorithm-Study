5c2b0be6bb7573d2eceaa48336a59aca06b73fa3
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-21 16:47:57 +0900

    [Silver V] Title: 분수찾기, Time: 164 ms, Memory: 80056 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#define MAX 4472
+#define XMAX 10000000
+using namespace std;
+int main(void) {
+    int x=1,y=1,d=0;
+    int dx[2]={-1,1};
+    int dy[2]={1,-1};
+    int A[MAX+1][MAX+1]={};
+    for (int i=1;i<=XMAX;i++){
+        A[x][y]=i;
+        if (x+dx[d%2]!=0 && y+dy[d%2]!=0){
+            x+=dx[d%2];
+            y+=dy[d%2];
+        }
+        else {
+            if (d%2==0){
+                y++;
+            }
+            else {
+                x++;
+            }
+            d++;
+        }
+    }
+    int n;
+    cin >> n;
+    for (int i=1;i<=MAX;i++){
+        for (int j=1;j<=MAX;j++){
+            if (A[i][j]==n){
+                cout << i<< '/' << j;
+                return 0;
+            }
+            else if (A[i][j]>n){
+                break;
+            }
+        }
+    }
+}
