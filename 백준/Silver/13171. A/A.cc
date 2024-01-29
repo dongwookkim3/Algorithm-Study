2cd0ab177eb5a56385527fe4d25e3c6ced7a3596
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-29 18:01:24 +0900

    [Silver III] Title: A, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13171. A/A.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+long long bpow(long long A,long long B,long long C){
+    if (B==1) return A%C;
+    long long T=bpow(A,B/2,C)%C;
+    if (B%2==0) return T*T%C;
+    else return T*T%C*A%C;
+}
+int main(){
+    long long A,B,C=1000000007;
+    cin >> A >> B;
+    cout << bpow(A%C,B,C);
+    return 0;
+}
