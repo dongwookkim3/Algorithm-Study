954d6664929036978d90445d6633858e879c3900
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-04 02:23:42 +0900

    [Silver II] Title: 마지막 팩토리얼 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define MOD 1000000
+using namespace std;
+int main(){
+    long long n,m=1;
+    cin >> n;
+    for (long long i=2;i<=n;i++){
+        m*=i;
+        while(m%10==0){
+            m/=10;
+        }
+        m%=MOD;
+    }
+    cout << m%10;
+    return 0;
+}
