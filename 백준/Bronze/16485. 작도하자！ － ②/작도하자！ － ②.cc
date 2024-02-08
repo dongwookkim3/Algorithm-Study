f6fd7f45ff409ae02ca8fa4519f7729cb6939bde
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-09 01:43:23 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 0 ms, Memory: 2020 KB, Score: 15 point -BaekjoonHub

--- 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.cc
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.cc
@@ -4,7 +4,7 @@ int main(){
     int A,B;
     cin >> A >> B;
     cout << fixed;
-    cout.precision(6);
-    cout << double(A/B);
+    cout.precision(9);
+    cout << (double)A/B;
     return 0;
 }
