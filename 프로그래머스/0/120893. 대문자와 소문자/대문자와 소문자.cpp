265848fb41493b531afb40af2b7008662227be9a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-09 14:02:04 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+string solution(string my_string){
+    for (int i=0;i<my_string.length();i++){
+        if (my_string[i]>90){
+            my_string[i]-=32;
+        }
+        else {
+            my_string[i]+=32;
+        }
+    }
+    return my_string;
+}
