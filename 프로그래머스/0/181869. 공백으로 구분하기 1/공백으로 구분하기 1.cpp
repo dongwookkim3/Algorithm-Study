c393952370c634af217392af9f917e8b4d2a8b49
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-09 19:42:58 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+vector<string> solution(string my_string){
+    vector<string> A;
+    string B;
+    istringstream iss(my_string);
+    while(getline(iss,B,' ')){
+        A.push_back(B);
+    }
+    return A;
+}
