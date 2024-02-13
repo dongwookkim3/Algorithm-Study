3259abf86bab15022d7d00c9dbbb0865108c5f17
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-14 08:20:25 +0900

    [Silver III] Title: N과 M (1), Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> A,B;
+void backtracking (int n,int m){
+    if (m==0){
+        for (int i=0;i<B.size();i++){
+            cout << B[i] << ' ';
+        }
+        cout << "\n";
+        B.pop_back();
+        return;
+    }
+    for (int i=0;i<n;i++){
+        bool f=true;
+        for (int j=0;j<B.size();j++){
+            if (A[i]==B[j]){
+                f=false;
+                break;
+            }
+        }
+        if (f){
+            B.push_back(A[i]);
+            backtracking(n,m-1);
+        }
+    }
+    B.pop_back();
+}
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for (int i=1;i<=n;i++){
+        A.push_back(i);
+    }
+    backtracking(n,m);
+    return 0;
+}
