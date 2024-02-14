174b15c7ad5bebd233854a37660c5d22e9455a9b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-14 09:09:48 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 8
+using namespace std;
+vector<int> A,B;
+void backtracking (int n,int m){
+    if (m==0){
+        for (int i=0;i<B.size();i++){
+            cout << B[i] << ' ';
+        }
+        cout << "\n";
+        B.pop_back();
+        return;
+    }
+    int v=-1;
+    for (int i=0;i<n;i++){
+        if (v!=A[i]){
+            if (!B.empty() && A[i]<B.back()){
+                continue;
+            }
+            v=A[i];
+            B.push_back(A[i]);
+            backtracking(n,m-1);
+        }
+    }
+    B.pop_back();
+}
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for (int i=1;i<=n;i++){
+        int a;
+        cin >> a;
+        A.push_back(a);
+    }
+    sort(A.begin(),A.end());
+    backtracking(n,m);
+    return 0;
+}
