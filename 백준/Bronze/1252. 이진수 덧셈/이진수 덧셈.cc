cacd897e2dd80cacb654aea7ae4e8e9c0e19c2e9
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-14 12:05:09 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    string A,B;
+    cin >> A >> B;
+    if (A.length()<B.length()){
+        string t=A;
+        A=B;
+        B=t;
+    }
+    reverse(A.begin(),A.end());
+    reverse(B.begin(),B.end());
+    for (int i=0;i<B.length();i++){
+        if (B[i]-48){
+            A[i]++;
+        }
+    }
+    for (int i=0;i<A.length();i++){
+        if (A[i]>=50){
+            A[i]-=2;
+            if (i==A.length()-1){
+                A.push_back('1');
+            }
+            else {
+                A[i+1]++;
+            }
+        }
+    }
+    while(A.back()=='0'){
+        A.pop_back();
+    }
+    reverse(A.begin(),A.end());
+    if (A.empty()) cout << 0;
+    else cout << A;
+    return 0;
+}
