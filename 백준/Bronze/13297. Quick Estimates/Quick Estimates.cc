db205100039237cbf3fda842728783c24ff81cf8
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-22 21:24:33 +0900

    [Bronze III] Title: Quick Estimates, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string A;
+        cin >> A;
+        cout << A.length() << "\n";
+    }
+    return 0;
+}
