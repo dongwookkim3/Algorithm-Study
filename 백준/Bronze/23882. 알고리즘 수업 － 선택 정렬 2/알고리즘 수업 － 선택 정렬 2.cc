a56475b407bdeb4e35644497b11af13409e763a4
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-23 23:23:02 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#define MAX 10000
+using namespace std;
+int main(){
+    int A[MAX+1]={};
+    int n,k;
+    cin >> n >> k;
+    for (int i=1;i<=n;i++){
+        cin >> A[i];
+    }
+    for (int i=n;i>=1;i--){
+        int m=A[i],mindex=0;
+        for (int j=1;j<i;j++){
+            if (A[j]>m){
+                m=A[j];
+                mindex=j;
+            }
+        }
+        if (mindex){
+            int t=A[i];
+            A[i]=A[mindex];
+            A[mindex]=t;
+            k--;
+            if (!k){
+                for (int j=1;j<=n;j++){
+                    cout << A[j] << ' ';
+                }
+                return 0;
+            }
+        }
+    }
+    cout << -1;
+    return 0;
+}
