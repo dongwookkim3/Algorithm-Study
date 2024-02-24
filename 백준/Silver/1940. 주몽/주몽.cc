034f60afdddbd49bc2861889a5ba95e99a3dd3a8
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-24 17:24:16 +0900

    [Silver IV] Title: 주몽, Time: 28 ms, Memory: 41092 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#define MAX 10000000
+using namespace std;
+int main(){
+    vector<int> A;
+    int B[MAX+1]={};
+    int n,m=0,x;
+    cin >> n >> x;
+    while(n--){
+        int a;
+        cin >> a;
+        A.push_back(a);
+        B[a]++;
+    }
+    for (int i=0;i<A.size();i++){
+        if (x-A[i]>=0){
+            m+=B[x-A[i]];
+        }
+    }
+    cout << m/2;
+    return 0;
+}
