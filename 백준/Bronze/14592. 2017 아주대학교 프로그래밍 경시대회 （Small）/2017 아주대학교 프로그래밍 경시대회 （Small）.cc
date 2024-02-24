0287719f78e6df2c7594f85c3349d4e50dcc6ff1
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-25 04:38:03 +0900

    [Bronze III] Title: 2017 아주대학교 프로그래밍 경시대회 (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14592. 2017 아주대학교 프로그래밍 경시대회 （Small）/2017 아주대학교 프로그래밍 경시대회 （Small）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 3
+using namespace std;
+struct S{
+    int A;
+    int B;
+    int C;
+    int D;
+};
+bool compare(S &A,S &B){
+    if (A.A==B.A && A.B==B.B) return A.C<B.C;
+    else if (A.A==B.A) return A.B<B.B;
+    return A.A>B.A;
+}
+int main(){
+    S A[MAX]={};
+    int n;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> A[i].A >> A[i].B >> A[i].C;
+        A[i].D=i+1;
+    }
+    sort(A,A+n,compare);
+    cout << A[0].D;
+    return 0;
+}
