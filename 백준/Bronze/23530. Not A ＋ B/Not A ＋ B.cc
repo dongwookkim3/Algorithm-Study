a38e42a19c55f5d4d864cc505c9a1aa5d4be4b48
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-26 00:26:48 +0900

    [Bronze III] Title: Not A + B, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int A,B;
+        cin >> A >> B;
+        cout << 1 << "\n";
+    }
+    return 0;
+}
