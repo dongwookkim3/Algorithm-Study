6e52cc8b470584ac30920c32d6e7fea2c2717c70
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-26 01:17:56 +0900

    [Bronze II] Title: 年齢の差 (Age Difference), Time: 204 ms, Memory: 6036 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.cc
+++ 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.cc
@@ -5,13 +5,16 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int> A(n);
+    vector<long long> A;
     for (int i=0;i<n;i++){
-        cin >> A[i];
+        long long a;
+        cin >> a;
+        A.push_back(a);
     }
-    vector<int> B=A;
+    vector<long long> B=A;
+    sort(B.begin(),B.end());
     for (int i=0;i<n;i++){
-        cout << max(abs(A[i]-*max_element(B.begin(),B.end())),abs(A[i]-*min_element(B.begin(),B.end()))) << "\n";
+        cout << max(abs(A[i]-B.front()),abs(A[i]-B.back())) << "\n";
     }
     return 0;
 }
