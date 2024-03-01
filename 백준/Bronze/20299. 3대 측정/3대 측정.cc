d341a794835bce82b55d39119ed73462947b25b1
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-02 02:03:11 +0900

    [Bronze II] Title: 3대 측정, Time: 180 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.cc
+++ 백준/Bronze/20299. 3대 측정/3대 측정.cc
@@ -3,6 +3,9 @@
 #include <algorithm>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n,m,k;
     cin >> n >> m >> k;
     vector<int> A;
