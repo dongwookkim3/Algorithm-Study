33f7780d77533bb475163ca8bf530248ee8ee9f3
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-02 04:30:56 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+string solution(string my_string, string alp){
+    for (int i=0;i<my_string.length();i++){
+        if (my_string[i]==alp.front()){
+            my_string[i]-=32;
+        }
+    }
+    return my_string;
+}
