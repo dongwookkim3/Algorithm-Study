5a3a18401235a70beb0b3d283d4172620696bd28
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-03 19:16:28 +0900

    [Bronze III] Title: 스케이트보드, Time: 68 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Bronze/28417. 스케이트보드/스케이트보드.cc
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.cc
@@ -3,6 +3,9 @@
 #include <algorithm>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     vector<int> A;
     int n;
     cin >> n;
