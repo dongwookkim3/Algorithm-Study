40414ff30017ee4379b3617800375939f7e18647
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-08 19:25:50 +0900

    [Bronze II] Title: 絶対階差数列 (Sequence of Absolute Differences), Time: 8 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27542. 絶対階差数列 （Sequence of Absolute Differences）/絶対階差数列 （Sequence of Absolute Differences）.cc
+++ 백준/Bronze/27542. 絶対階差数列 （Sequence of Absolute Differences）/絶対階差数列 （Sequence of Absolute Differences）.cc
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
-    int A;
     int n;
-    cin >> n >> A;
-    while(--n){
-        int a;
-        cin >> a;
-        A=abs(A-a);
+    cin >> n;
+    vector<int> A(n);
+    for (int i=0;i<n;i++){
+        cin >> A[i];
     }
-    cout << A;
+    while(A.size()!=1){
+        vector<int> B;
+        for (int i=0;i<A.size()-1;i++){
+            B.push_back(abs(A[i]-A[i+1]));
+        }
+        A=B;
+    }
+    cout << A.front();
     return 0;
 }
