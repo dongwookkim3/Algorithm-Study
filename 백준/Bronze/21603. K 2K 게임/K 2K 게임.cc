017bb728e4c21ac2e34841483eb6cb87d6d0c6bf
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-13 00:32:11 +0900

    [Bronze III] Title: K 2K 게임, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> A;
+    int n,m;
+    cin >> n >> m;
+    for (int i=1;i<=n;i++){
+        if (i!=m%10 && i!=(m*2)%10){
+            A.push_back(i);
+        }
+    }    
+    cout << A.size() << "\n";
+    for (int i=0;i<A.size();i++){
+        cout << A[i] << ' ';
+    }
+    return 0;
+}
