01fa40046a45e9275bde7ca2098dd15ffc6ed00a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-22 16:08:27 +0900

    [Silver IV] Title: Selection Sum, Time: 444 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/13229. Selection Sum/Selection Sum.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> A(n+1);
+    for (int i=1;i<=n;i++){
+        cin >> A[i];
+        A[i]+=A[i-1];
+    }
+    int m;
+    cin >> m;
+    while(m--){
+        int a,b;
+        cin >> a >> b;
+        cout << A[b+1]-A[a] << "\n";
+    }
+    return 0;
+}
